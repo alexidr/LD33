87b3c0c2c37509b6cf0039bdfa1c5b57f0f9dd27
Alex <alex@alex>
2015-08-23 20:34:58 +0300

    asd

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour 
+{
+	public float movingSpeed = 5.0f;
+	public float health = 100.0f;
+
+	[HideInInspector]
+	public GameObject targetObject;
+
+	protected bool dead;
+
+	virtual public void OnGettingHit(float damage)
+	{
+		health -= damage*Time.deltaTime;
+	}
+	
+	virtual public void PlayDeath()
+	{
+	}
+}
