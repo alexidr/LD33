cc2f17520daa3c166e5b20052db50de62492479d
Alex <alex@alex>
2015-08-23 13:16:44 +0300

    up

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject follow;
+	public float speed;
+
+	Vector3 initialPos;
+	Vector3 initialOffset;
+	// Use this for initialization
+	void Start () {
+	
+		initialPos = transform.position;
+		initialOffset = follow.transform.position - initialPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 target = initialPos;
+		target.x = follow.transform.position.x - initialOffset.x;
+
+		transform.position = Vector3.Lerp(transform.position, target, speed*Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterController : MonoBehaviour 
+{
+	bool doingStep = false;
+	bool doingShake = false;
+
+	public float stepTime;
+	public float stepLength;
+	public float gunRotationTime;
+
+	public GameObject gun;
+
+	Vector3 target;
+
+	// Use this for initialization
+	void Start () 
+	{
+		target = transform.position;
+	}
+
+	void DoStep()
+	{
+		if (Input.GetKey (KeyCode.D))
+		{
+			target = transform.position + Vector3.right*stepLength;
+
+			if(!doingShake)
+			{
+				Hashtable shakeParams = new Hashtable();
+				shakeParams["amount"] = new Vector3(0.0f, 0.0f, 2.0f);
+				shakeParams["oncomplete"]  = "OnShakeDone";
+				shakeParams["time"] = stepTime * 0.5f;
+				iTween.ShakeRotation(gameObject, shakeParams);
+
+				doingShake = true;
+			}
+		}
+
+		Hashtable moveParams = new Hashtable();
+		moveParams["position"] = target;
+		moveParams["oncomplete"]  = "OnStepDone";
+		moveParams["time"] = stepTime;
+		iTween.MoveUpdate(gameObject, moveParams);
+    }
+
+	Vector3 GetMouseWorld()
+	{
+		var v3 = Input.mousePosition;
+		v3.z = transform.position.z - Camera.main.transform.position.z;
+		return Camera.main.ScreenToWorldPoint(v3);
+	}
+    // Update is called once per frame
+	void Update () 
+	{
+		//if (!doingStep)
+		DoStep();
+
+		Vector3 dir = GetMouseWorld() - gun.transform.position;
+		dir.z = 0.0f;
+		dir.Normalize();
+		//Debug.LogError(dir);
+		//iTween.RotateUpdate(gun, Quaternion.LookRotation(dir).eulerAngles, gunRotationTime);
+
+		gun.transform.up = -dir;
+	}
+
+	void OnStepDone()
+	{
+	}
+
+	void OnShakeDone()
+	{
+		doingShake = false;
+	}
+}
